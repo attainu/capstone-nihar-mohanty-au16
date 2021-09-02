69251e503f8532b12e39ae0477cf68a2cdf7c33b
Debasis Pattanaik <67583843+debasis-pattanaik-au16@users.noreply.github.com>
2021-09-02 21:37:30 +0530

    Delete backend-api directory

--- backend-api/routes/auth.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const authController = require('../controllers/authController');
-
-router.post('/register', authController.signup);
-
-router.post('/login', authController.login);
-
-module.exports = router;
